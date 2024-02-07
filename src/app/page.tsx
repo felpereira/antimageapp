339ebd9e197f75b55f3fbf278c7f7387508b62c1
cb.felipe <cb.felipe@gmail.com>
2024-02-06 22:44:23 -0300

    ultimo detalhe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,43 +4,21 @@ import React from 'react';
 
 import SideBar from './[user]/components/SideBar';
 import { authOptions } from './api/auth/[...nextauth]/route';
-import styles from './page.module.css';
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
     const nomeUsuario = session?.user;
 
     return (
-        <main className={styles.main}>
-            <SideBar />
-            <Link
-                href="/sing-up"
-                className={styles.link}
-            >
-                Não tem uma conta? Registre-se.
-            </Link>
+        <main>
+            <Link href="/sing-up">Não tem uma conta? Registre-se.</Link>
             <br></br>
-            <Link
-                href="/sing-in"
-                className={styles.link}
-            >
-                Entrar
-            </Link>
+            <Link href="/sing-in">Entrar</Link>
             <br></br>
-            <Link
-                href="/forgot-password"
-                className={styles.link}
-            >
-                Esqueceu a senha
-            </Link>
+            <Link href="/forgot-password">Esqueceu a senha</Link>
             <br></br>
             {session && (
-                <Link
-                    href={`/${nomeUsuario?.toString()}`}
-                    className={styles.link}
-                >
-                    Painel
-                </Link>
+                <Link href={`/${nomeUsuario?.toString()}`}>Painel</Link>
             )}
         </main>
     );
