1a796aff2d62164a313bcd379ef589706fc79b85
cb.felipe <cb.felipe@gmail.com>
2024-02-11 13:16:45 -0300

    .

--- src/app/[user]/components/SideBar/SideBar.tsx
+++ src/app/[user]/components/SideBar/SideBar.tsx
@@ -15,39 +15,14 @@ interface SideBarProps {
 }
 
 export default function SideBar({ nomeUsuario }: Readonly<SideBarProps>) {
-    const { openSideMenu, windowSize } = useContext(LayoutContext);
+    const { openSideMenu } = useContext(LayoutContext);
 
-    const exibirSideMenu =
-        windowSize.width < TAMANHO_MINIMO_SIDEBAR_VISIVEL &&
-        windowSize.width != 0;
-
-    let styleSideBar = {};
-
-    if (exibirSideMenu) {
-        styleSideBar = {
-            position: exibirSideMenu ? 'absolute' : '',
-            width: exibirSideMenu ? '0px' : '15vw',
-            margin: exibirSideMenu ? '0px' : ' 5px 5px 5px 10px',
-            padding: exibirSideMenu ? '0px' : '1rem',
-            visibility: exibirSideMenu ? 'hidden' : 'visible',
-            transition: 'width 1s ease'
-        };
-    }
-
-    if (openSideMenu && exibirSideMenu) {
-        styleSideBar = {
-            position: 'absolute',
-            width: '50vw',
-            margin: ' 5px 5px 5px 5px',
-            transition: 'width 1s ease'
-        };
-    }
+    const classNameSidebar = openSideMenu
+        ? `${styled.sidebar} ${styled.exibirSidebarProCima}`
+        : `${styled.sidebar}`;
 
     return (
-        <div
-            style={styleSideBar}
-            className={styled.sidebar}
-        >
+        <div className={classNameSidebar}>
             <SideMenuLogo />
             <SideBarInforUser usuario={nomeUsuario} />
             <SidebarProperties usuario={nomeUsuario} />
